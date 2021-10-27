8548402767c6b52f756f047e535a473f5664d8b9
Mohammad Moradyar <moradyar.mohammad@gmail.com>
2021-10-27 19:38:43 +0200

    Sixth

--- app/src/main/java/com/moradyar/sdkarchitecturetemplate/RootApp.kt
+++ app/src/main/java/com/moradyar/sdkarchitecturetemplate/RootApp.kt
@@ -1,15 +1,10 @@
 package com.moradyar.sdkarchitecturetemplate
 
 import android.app.Application
-import com.moradyar.sdkinitializingenabler.core.FireworkSdk
 
 class RootApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        FireworkSdk.inti(
-            hostClientId = "HostClientId",
-            hostUserId = "HostUserId"
-        )
     }
 }
